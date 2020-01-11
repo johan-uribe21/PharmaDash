e5e5784486e9ee72d133272d27c210e7fc4ae857
Johan Uribe <johanandresuribe@gmail.com>
2020-01-10 23:31:05 -0500

    Updated client state fake data

--- client/src/store/pharmaStore/state.js
+++ client/src/store/pharmaStore/state.js
@@ -3,25 +3,37 @@ export default function() {
     user: "",
     ordersToday: [
       {
-        patientName: "Jill Johnson",
-        patientAddress: {
-          street: "112 Forest Ln.",
-          city: "Columbus",
-          stateAbr: "OH",
-          zipcode: "43203"
-        },
-        pharmacy: {
-          name: "BetterRx",
-          street: "1275 Kinnear Road",
-          city: "Columbus",
-          stateAbr: "OH",
-          zipcode: "43201"
-        },
-        rxIds: ["RX123", "RX321"],
-        orderId: "123456",
-        pickupDate: "01-10-2020",
-        pickupTime: "14:30",
-        active: true
+        order: {
+          patient: {
+            name: "Jill Johnson",
+            street: "112 Forest Ln.",
+            city: "Columbus",
+            stateAbr: "OH",
+            zipcode: "43203"
+          },
+          pharmacy: {
+            name: "BetterRx",
+            street: "1275 Kinnear Road",
+            city: "Columbus",
+            stateAbr: "OH",
+            zipcode: "43201"
+          },
+          courier: {
+            name: "Same Day Delivery",
+            street: "900 Trenton Lane",
+            city: "Trenton",
+            stateAbr: "NJ",
+            zipcode: "08536",
+            pharmacies: [1, 2]
+          },
+          orderId: "123456",
+          rxIds: ["RX123", "RX321"],
+          pickupDate: "01-10-2020",
+          pickupTime: "14:30",
+          active: true,
+          delivered: false,
+          deliverable: true
+        }
       },
       {
         patientName: "Gene Greyson",
